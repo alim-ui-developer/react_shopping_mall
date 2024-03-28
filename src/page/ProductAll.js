556460fedc590343f82c570cb891e32c295e13cf
alim <dahye_a@naver.com>
2024-03-28 15:39:13 +0900

    data url 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,8 +5,8 @@ import ProductCard from '../component/ProductCard';
 const ProductAll = ({}) => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    // let url = 'https://my-json-server.typicode.com/shanghanrun/hm_shopping/products';
-    let url = 'http://localhost:5000/products';
+    let url = 'https://my-json-server.typicode.com/shanghanrun/hm_shopping/products';
+    // let url = 'http://localhost:5000/products';
     let response = await fetch(url);
     let data = await response.json();
     setProductList([...data]);
