533824ee6776af80756811d9e102e3aebcd53fa6
alim <dahye_a@naver.com>
2024-04-03 01:08:44 +0900

    first commit

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,18 @@
+let initialState = {
+  productList:[],
+  selectedItem: null,
+}
+
+function productReducer(state = initialState, action){
+  let { type, payload } = action;
+  switch (type){
+    case "GET_PRODUCT_SECCESS":
+      return { ...state, productList:payload.data };
+      case "GET_PRODUCT_DETAIL":
+        return { ...state, selectedItem:payload.data };
+    default:
+      return { ...state }
+  }
+}
+
+export default productReducer;
